b845a7b4e4b94c4d239324ef66d9e41cff8f1666
andresvilla4 <andresmauriciovillacobgarrido@hotmail.com>
2021-04-28 12:45:36 -0500

    Segundo commit

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const getLugarLatLgn = async(direccion) => {
 
     let encodeUrl = encodeURI(direccion);
-    const APIKEY = 'pk.eyJ1IjoiYW5kcmVzdmlsbGFjb2IiLCJhIjoiY2tvMWoxeXh5MGx4YTJwcDlxM3pvNnJhdSJ9.YnJMGgHCa1yE4wmzD6ZvBg';
+    const APIKEY = '<API_KEY>_MapBox';
 
     let resp = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeUrl}.json?access_token=${APIKEY}`)
 
